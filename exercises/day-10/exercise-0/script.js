a2c8440f2e4cc07239aac08047e17b55dd4409c2
Tom Huynh <tom.huynh@rmit.edu.vn>
2024-03-24 23:00:57 +0700

    Update script.js

--- exercises/day-10/exercise-0/script.js
+++ exercises/day-10/exercise-0/script.js
@@ -1,3 +1,41 @@
 console.log("Hello world!");
 
 // Add your js code below here!
+
+/*
+Use querySelector or querySelectorAll to select:
+- Banner image.
+- H1 text.
+- All list items.
+- Done items.
+- Not-done items 
+- All buttons
+- Done button.
+- Not-done button.
+*/
+
+
+/*
+Using JS to make these changes:
+- Get h1 text and change its text value and color.
+- Get second list item text and change its value.
+- Get the image link url and change it to another link.
+- Change all list items to be done.
+- Change all list items from done to be not done and vice versa.
+*/
+
+/*
+Using JS to make these changes:
+- Get h1 text and change its text value and color.
+- Get second list item text and change its value.
+- Get the image link url and change it to another link.
+- Change all list items to be done.
+- Change all list items from done to be not done and vice versa.
+*/
+
+/*
+Using addEventListener() to handle these events:
+- Clicking on the button “Done All” makes all list items to be done.
+- Clicking on the button “Not Done All” makes all list items to be done.
+- Clicking on any item list will make the item toggle from done to not done or vice versa.
+*/
